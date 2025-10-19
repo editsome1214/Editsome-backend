4dd369e193aed0f544ea68d6c62da570097037a0
editsome1214 <hbhattihamid@gmail.com>
2025-10-19 04:03:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,5 +19,5 @@ def merge_pdfs():
     merger.close()
     return send_file(output_path, as_attachment=True)
 
-if __name__ == '_main_':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
